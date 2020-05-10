f3b684d9c434cfd72abdd9c40a90f53f83f2d0ce
wolflo <wardnick@protonmail.com>
2020-05-10 17:23:07 +0400

    Initial commit

---
+++ src/BigConstructor.t.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+import "ds-test/test.sol";
+
+contract BigConstructor is DSTest {
+    bytes internal bar = hex'00';
+
+    function setUp() public {}
+    function test() public {}
+}
